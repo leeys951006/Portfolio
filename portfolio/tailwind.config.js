f58074de1f43c93f238304920c5958f044641a18
이연승 <leeys951006@gmail.com>
2024-09-05 11:53:12 +0900

    기본 설정

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // Tailwind가 app 디렉토리 내 파일을 스캔하도록 경로 설정
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
