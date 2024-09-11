ffb7e00ec5332853dd9e978241abf70af572a4d4
이연승 <leeys951006@gmail.com>
2024-09-11 17:04:46 +0900

    project-group 시작

--- portfolio/app/project-group/page.tsx
+++ portfolio/app/project-group/page.tsx
@@ -1,16 +1,15 @@
 'use client';
 
-import { useContext} from 'react';
+import { useContext } from 'react';
 import { BrightModeContext } from '../ClientProvider';
 import BalloonCursor from '../../components/BalloonCursor';
 
 export default function HomePage() {
-  const { isBrightMode, } = useContext(BrightModeContext);
-  
+  const { isBrightMode } = useContext(BrightModeContext);
 
   return (
     <div className={`min-h-screen ${isBrightMode ? 'bg-[#f0f0f0]' : 'bg-white'} flex flex-col items-center justify-between`}>
-     
+      <div className={`w-full min-h-screen flex flex-col items-center justify-center space-y-8 ${isBrightMode ? 'bg-[#F7B033]' : 'bg-[#333333]'}`}></div>
       <BalloonCursor />
     </div>
   );
