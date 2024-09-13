349b9871c96350a26a6fde043f37c12a20930f9a
이연승 <leeys951006@gmail.com>
2024-09-13 13:04:48 +0900

    로그인 버튼 추가

--- portfolio/app/Header.tsx
+++ portfolio/app/Header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Header() {
-  const { isBrightMode } = useContext(BrightModeContext); // toggleBrightMode 제거
+  const { isBrightMode } = useContext(BrightModeContext);
   const [isMenuOpen, setIsMenuOpen] = useState(false); // 사이드바 상태 관리
 
   const toggleMenu = () => {
@@ -58,6 +58,15 @@ export default function Header() {
           >
             프로젝트
           </Link>
+          {/* 로그인 링크 추가 */}
+          <Link
+            href="/login"
+            className={`font-semibold ${isBrightMode ? 'text-[#F7B033]' : 'text-[#333333]'} hover:underline ${
+              isBrightMode ? 'hover:text-[#FFD700]' : 'hover:text-[#666666]'
+            } transition-colors duration-200`}
+          >
+            로그인
+          </Link>
         </nav>
 
         {/* 사이드바 메뉴 (모바일용) */}
@@ -91,6 +100,14 @@ export default function Header() {
               className={`block px-4 py-2 font-semibold hover:text-gray-500 transition-colors duration-200 ${isBrightMode ? 'text-[#F7B033]' : 'text-[#333333]'}`}
             >
               프로젝트
+            </Link>
+            {/* 로그인 링크 추가 */}
+            <Link
+              href="/login"
+              onClick={toggleMenu}
+              className={`block px-4 py-2 font-semibold hover:text-gray-500 transition-colors duration-200 ${isBrightMode ? 'text-[#F7B033]' : 'text-[#333333]'}`}
+            >
+              로그인
             </Link>
           </nav>
         </div>
